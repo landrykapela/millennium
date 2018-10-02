2b402484044de01dc7fc6923529eb44d7a9d2f02
Landry Kapela <landrykapela@gmail.com>
2018-10-02 15:06:59 +0300

    Remove firebase web api key

--- app/src/main/java/tz/co/neelansoft/millennium/data/Config.java
+++ app/src/main/java/tz/co/neelansoft/millennium/data/Config.java
@@ -1,7 +1,7 @@
 package tz.co.neelansoft.millennium.data;
 
 public class Config {
-    public static final String WEB_CLIENT_KEY = "951181493881-6i7u5olkoibt6bgfp7no56du81gdot3c.apps.googleusercontent.com";
+    public static final String WEB_CLIENT_KEY = "";
     public static final String FIREBASE_DB_REFERENCE = "millennium";
     public static final String BASE_URL = "https://elimukwanza.ac.tz/android/api/";
     public static final String BOOKS_URL = BASE_URL.concat("books/");
